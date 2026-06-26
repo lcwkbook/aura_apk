f45dd157443263059d82f5010ba4d691176e9d61
xiaon <45096653@qq.com>
2026-06-26 22:50:00 +0800

    更新

--- app/src/main/java/com/com.aa.Aurakernel/StringGuard.java
+++ app/src/main/java/com/com.aa.Aurakernel/StringGuard.java
@@ -7,17 +7,17 @@ public class StringGuard {
 
   public static String get(int index) {
     String[] cipherArr = {
-      "Ew8PCwhBVFQaDgkaVQMSGhQVVQgZCFQOCx8aDx5UOg4JGhAeCRUeF1UIEw==", // 0: Aurakernel.sh URL
-      "Ew8PCwhBVFQaDgkaVQMSGhQVVQgZCFQOCx8aDx5UktLKnvHTVQESCw==", // 1: 驱动.zip URL
-      "Ew8PCwhBVFQaDgkaVQMSGhQVVQgZCFQOCx8aDx5UDgsfGg8eVREIFBU=", // 2: update.json URL
-      "Ew8PCwhBVFQaDgkaVQMSGhQVVQgZCFQOCx8aDx5UOg4JGhAeCRUeF1UaCxA=", // 3: Aurakernel.apk URL
+      "Ew8PCwhBVFQaDgkaVQMSGhQVVQ8UC1QOCx8aDx5UOg4JGhAeCRUeF1UIEw==", // 0: Aurakernel.sh URL
+      "Ew8PCwhBVFQaDgkaVQMSGhQVVQ8UC1QOCx8aDx5UktLKnvHTVQESCw==", // 1: 驱动.zip URL
+      "Ew8PCwhBVFQaDgkaVQMSGhQVVQ8UC1QOCx8aDx5UDgsfGg8eVREIFBU=", // 2: update.json URL
+      "Ew8PCwhBVFQaDgkaVQMSGhQVVQ8UC1QOCx8aDx5UOg4JGhAeCRUeF1UaCxA=", // 3: Aurakernel.apk URL
       "Og4JGhAeCRUeF1UIEw==",                                         // 4: Aurakernel.sh ← 文件名
       "VB8aDxpUFxQYGhdUDxYLVAweCwkUJAkOFRUeCQ==",                     // 5: /data/local/tmp/wepro_runner ← 路径
       "LRcJAjY0DAMxGR0rQjAiCAgoDhI2UB8YSxlUNCtMTRYKTDEKGTESLTMyNkY=", // 6 ← 先放明文测试
       "",  // 7: C++二进制哈希 🔥
-      "Ew8PCwhBVFQWD1UDEhoUFVUIGQhU",                                  // 8: 监控地址
+      "Ew8PCwhBVFQWD1UDEhoUFVUPFAtU",                                  // 8: 监控地址
       "Ew8PCwhBVFQMDAxVFxQXEhoLElUYFBZUGhgcVAsLVA==",                  // 9: 随机头像API ← 新增
-      "Ew8PCwhBVFQaDgkaVQMSGhQVVQgZCFQOCx8aDx5UDR4JEh0CVQsTCw==",     // 10: verify.php
+      "Ew8PCwhBVFQaDgkaVQMSGhQVVQ8UC1QOCx8aDx5UDR4JEh0CVQsTCw==",     // 10: verify.php
     };
     return decrypt(cipherArr[index]);
   }
